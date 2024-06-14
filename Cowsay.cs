4fb20dc57f5e804d4336b6c3098c1796dabc3d7f
Christian Benedict R. Gantuangco <gantuangcoc98@gmail.com>
2024-06-14 20:25:24 +0800

    rollback changes

--- Cowsay.cs
+++ Cowsay.cs
@@ -2,18 +2,15 @@ namespace Class;
 
 public class Cowsay
 {
+
     public event EventHandler<string> Reply;
     
-    private string Message = "";
-
     public void Say(string? Message)
     {
         if (Message is null)
             return;
-        
-        this.Message = Message;
 
-        Reply(this, this.Message);
+        Reply(this, Message);
     }
 
 }
